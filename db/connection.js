aa1577f7f3bbbc767219f00f379682fd51c2151d
CB_ATL_123_Ahkeim_Crenshaw <cb_atl_123_ahkeim_crenshaw@PSADMINs-Air-3.attlocal.net>
2018-11-15 22:23:51 -0500

    -X-, I got rid of the error and am now connected to mongoDB! Next objective.

--- db/connection.js
+++ db/connection.js
@@ -1,15 +1,21 @@
-require('dotenv').config()
+require('dotenv').config();
 const mongoose = require('mongoose')
 
-mongoose.connect('process.env.MONGODB_URI')
 
-mongoose.connection.once('open', () => {
-    console.log('Mongoose has connected to MongoDB!')
-})
-
-mongoose.connection.on('error', (error) => {
-    console.error(`MongoDB has had an error!!! ${error}`)
-    process.exit(-1)
-})
+// Connect to database
+if (process.env.MONGODB_URI) {
+    mongoose.connect(process.env.MONGODB_URI);
+}
+else {
+    mongoose.connect('mongodb://localhost/deckmaster');
+}
+mongoose.connection.on('error', function (err) {
+    console.error('MongoDB connection error: ' + err);
+    process.exit(-1);
+}
+);
+mongoose.connection.once('open', function () {
+    console.log("Mongoose has connected to MongoDB!");
+});
 
 module.exports = mongoose
