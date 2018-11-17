0ea2a15f6b43fcf57f0ef39a95a3117140084089
CB_ATL_123_Ahkeim_Crenshaw <cb_atl_123_ahkeim_crenshaw@PSADMINs-Air-3.attlocal.net>
2018-11-16 20:11:27 -0500

    #11: -X-! Got express and react running at the same time. Now to test routes and start creating components.

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.json())
 // app.use(express.static(__dirname + '/client/build/'))
 
 // app.get('/', (req, res) => {
-//     res.sendFile(__dirname + '/client/build/index')
+//     res.sendFile(__dirname + '/client/build/index.html')
 // })
 
 app.use('/', routes)
